b0a454c8e1c85afc0444bef54f03ea12be05fe53
JP Norair <jpnorair@indigresso.com>
2017-06-18 13:31:12 -0700

    Copyright revision

--- platform/stdc/platform_stdc.c
+++ platform/stdc/platform_stdc.c
@@ -1,4 +1,4 @@
-/* Copyright 2010-2012 JP Norair
+/* Copyright 2017 JP Norair
   *
   * Licensed under the OpenTag License, Version 1.0 (the "License");
   * you may not use this file except in compliance with the License.
