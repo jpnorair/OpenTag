71a8be76c8654ba3da86d1376734039ce6d7ec72
John Peter Norair <jpnorair@indigresso.com>
2022-06-02 18:21:29 -0700

    Add null project, which is a workaround to simplify dual-core debugging

---
+++ apps/null/stm32cube_wl55/CM0PLUS/Core/Src/main.c
@@ -0,0 +1,36 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  __disable_irq();
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1) { }
+}
+
+
